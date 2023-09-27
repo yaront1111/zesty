8fe5e375f39c7554b02ba23cb217e0cec93c0206
Yaron Torgeman <yaron.torgeman@cyberark.com>
2023-09-27 14:27:21 +0300

    CI

--- App/app/app.py
+++ App/app/app.py
@@ -3,16 +3,23 @@ import logging
 import os
 from botocore.exceptions import ClientError, ParamValidationError
 
-DYNAMODB_ENDPOINT = os.environ.get('DYNAMODB_ENDPOINT', 'http://localhost:8000')
-dynamodb = boto3.client('dynamodb', region_name='us-west-2', endpoint_url=DYNAMODB_ENDPOINT)
+DYNAMODB_ENDPOINT = os.environ.get("DYNAMODB_ENDPOINT", "http://localhost:8000")
+dynamodb = boto3.client(
+    "dynamodb", region_name="us-west-2", endpoint_url=DYNAMODB_ENDPOINT
+)
 
-logging.basicConfig(level=logging.INFO, filename='fetch_secret.log', filemode='a', format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    level=logging.INFO,
+    filename="fetch_secret.log",
+    filemode="a",
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
+
 
 def fetch_secret(codeName):
     try:
         response = dynamodb.get_item(
-            TableName='devops-challenge',
-            Key={'codeName': {'S': codeName}}
+            TableName="devops-challenge", Key={"codeName": {"S": codeName}}
         )
         logging.info(f"Fetched item from DynamoDB: {response}")
     except ClientError as e:
@@ -22,8 +29,8 @@ def fetch_secret(codeName):
         logging.error(f"ParamValidationError fetching from DynamoDB: {e}")
         return None
     else:
-        if 'Item' in response:
-            secret_code = response['Item']['secretCode']['S']
+        if "Item" in response:
+            secret_code = response["Item"]["secretCode"]["S"]
             logging.info(f"Successfully retrieved secret code: {secret_code}")
             return secret_code
         else:
--- App/app/router.py
+++ App/app/router.py
@@ -8,52 +8,65 @@ import logging
 import os
 
 # Initialize secrets
-with open('/run/secrets/api_token', 'r') as file:
+with open("/run/secrets/api_token", "r") as file:
     API_TOKEN = file.read().strip()
-with open('/run/secrets/flask_secret_key', 'r') as file:
+with open("/run/secrets/flask_secret_key", "r") as file:
     FLASK_SECRET_KEY = file.read().strip()
-with open('/run/secrets/flask_secret_key', 'r') as file:
+with open("/run/secrets/flask_secret_key", "r") as file:
     CODE_NAME = file.read().strip()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('FLASK_SECRET_KEY', 'fallbackSecretKey')
-s = Serializer(app.config['SECRET_KEY'])
-logging.basicConfig(level=logging.INFO, filename='audit.log', filemode='a', format='%(asctime)s - %(levelname)s - %(message)s')
+app.config["SECRET_KEY"] = os.environ.get("FLASK_SECRET_KEY", "fallbackSecretKey")
+s = Serializer(app.config["SECRET_KEY"])
+logging.basicConfig(
+    level=logging.INFO,
+    filename="audit.log",
+    filemode="a",
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
 
 # Initialize rate limiter
 limiter = Limiter(
     app=app,
     key_func=get_remote_address,
-    default_limits=["5 per minute", "5 per second"]
+    default_limits=["5 per minute", "5 per second"],
 )
 
-@app.route('/health', methods=['GET'])
+
+@app.route("/health", methods=["GET"])
 @limiter.limit("5 per minute")
 def health_check():
-    logging.info('Health check requested.')
-    return jsonify({"status": "healthy", "container": os.environ.get("CONTAINER_REGISTRY", "unknown")})
+    logging.info("Health check requested.")
+    return jsonify(
+        {
+            "status": "healthy",
+            "container": os.environ.get("CONTAINER_REGISTRY", "unknown"),
+        }
+    )
 
-@app.route('/token', methods=['GET'])  # Generate a token
+
+@app.route("/token", methods=["GET"])  # Generate a token
 def get_token():
-    token = s.dumps({'token': API_TOKEN})
+    token = s.dumps({"token": API_TOKEN})
     return jsonify(token=token)
 
-@app.route('/secret', methods=['GET'])
+
+@app.route("/secret", methods=["GET"])
 @limiter.limit("5 per second")
 def get_secret():
-    auth_header = request.headers.get('Authorization')
+    auth_header = request.headers.get("Authorization")
     if not auth_header:
-        logging.warning('Missing Authorization header.')
+        logging.warning("Missing Authorization header.")
         abort(403, description="Missing Authorization header")
 
     auth_parts = auth_header.split(" ")
     if len(auth_parts) != 2 or auth_parts[0] != "Bearer" or auth_parts[1] != API_TOKEN:
-        logging.warning('Invalid API token.')
+        logging.warning("Invalid API token.")
         abort(403, description="Invalid API token")
 
-    codeName = request.args.get('codeName')
+    codeName = request.args.get("codeName")
     if codeName != CODE_NAME:
-        logging.warning(f'Invalid codeName attempt: {codeName}')
+        logging.warning(f"Invalid codeName attempt: {codeName}")
         abort(403, description="Invalid codeName.")
 
     secret_code = fetch_secret(codeName)
@@ -63,5 +76,5 @@ def get_secret():
         abort(404, description="Secret code not found.")
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
